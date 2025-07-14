7d57e2ce45c11998b06f1a225f0d9d49ab6e4f24
VladislavMisukevich <v4388452@gmail.com>
2025-07-15 00:32:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Force reinstalling dependencies..."
+pip install --force-reinstall -r requirements.txt
+
 echo "Applying database migrations..."
 python database.py
+
 echo "Starting bot..."
 python main.py
